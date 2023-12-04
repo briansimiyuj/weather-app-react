228b0f541c2d73336727236ff8c9fa6e2224fbab
Brian Simiyu <briansimiyuj@gmail.com>
2023-12-04 13:54:20 +0300

    Weather image markup

--- src/Script.jsx
+++ src/Script.jsx
@@ -1,10 +1,13 @@
 import Form from './components/Form'      
+import WeatherImage from './components/WeatherImage'
 
 function Script(){
   return (
     <div className="container">
 
       <Form/> 
+
+      <WeatherImage/>
     
     </div>
   )
---
+++ src/components/WeatherImage.jsx
@@ -0,0 +1,17 @@
+import clear from '../img/clear.png'
+
+const WeatherImage = () =>{
+
+    return(
+
+        <div className="weather-image">
+
+            <img src={clear} alt="weather-image"/>
+
+        </div>
+
+    )
+
+}
+
+export default WeatherImage
