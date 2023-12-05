697f5acfcf26a6ea92b9f5e8a19b559417a9690c
Brian Simiyu <briansimiyuj@gmail.com>
2023-12-05 12:51:59 +0300

    Controlled inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,20 @@
 import search from '../img/search.png'
+import { useState } from 'react'
 
 const Form = () =>{
+
+  const [location, setLocation] = useState('')
 
     return(
 
         <form>
 
-          <input type="text" placeholder="Search Location"/>
+          <input 
+            type="text"
+            placeholder="Search Location" 
+            value={location}
+            onChange={e => setLocation(e.target.value)}
+          />
 
 
           <button className="search-icon">
